b1646b49a443946600bbacd5864dc99ba1016010
SalomoCedric <salomokarst19@gmail.com>
2025-02-11 23:28:46 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,12 @@
 // Firebase Konfiguration
+// Firebase Konfiguration (ersetze mit deinen echten Daten)
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBX_GqLCg1yOD1aPjxHZ1bQgdTQ5B7trv8",
+  authDomain: "pftinder-79946.firebaseapp.com",
+  projectId: "pftinder-79946",
+  storageBucket: "pftinder-79946.firebasestorage.app",
+  messagingSenderId: "165850597415",
+  appId: "1:165850597415:web:84e698173e2f983c4d6602"
 };
 
 // Firebase initialisieren
