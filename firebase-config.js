1e617899415c01559de1035bebe8899bbf63a56d
SalomoCedric <salomokarst19@gmail.com>
2025-02-11 22:54:06 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 // firebase-config.js
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyBX_GqLCg1yOD1aPjxHZ1bQgdTQ5B7trv8",
   authDomain: "pftinder-79946.firebaseapp.com",
   projectId: "pftinder-79946",
@@ -7,4 +7,7 @@ export const firebaseConfig = {
   messagingSenderId: "165850597415",
   appId: "1:165850597415:web:84e698173e2f983c4d6602"
 };
+
+export { firebaseConfig };
+
 
