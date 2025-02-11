6e4b71a19d3bfda8692bc05206a06ab10c9cd1c5
SalomoCedric <salomokarst19@gmail.com>
2025-02-11 22:45:46 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,10 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { firebaseConfig } from "./firebase-config.js"; // firebaseConfig aus der richtigen Datei importieren
+import { firebaseConfig } from "./firebase-config.js"; // Firebase-Konfiguration importieren
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-// Firebase initialisieren
-const app = initializeApp(firebaseConfig);  // Hier wird die Firebase-App initialisiert
-const db = getFirestore(app); // Firestore-Dienst initialisieren
-const storage = getStorage(app); // Storage-Dienst initialisieren
+const app = initializeApp(firebaseConfig);  // Firebase-App initialisieren
+const db = getFirestore(app); // Firestore initialisieren
+const storage = getStorage(app); // Storage initialisieren
 
-// Exportiere die Firebase-Instanzen, damit du sie in anderen Dateien verwenden kannst
-export { db, storage };
+export { db, storage };  // Firebase-Dienste exportieren
