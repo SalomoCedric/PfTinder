938724128f78022b7f09010f8b4d0581937bab7b
SalomoCedric <salomokarst19@gmail.com>
2025-02-12 07:52:01 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,6 @@
+document.getElementById("login-btn").addEventListener("click", login);
+document.getElementById("register-btn").addEventListener("click", register);
+
 function login() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
@@ -27,3 +30,4 @@ function register() {
             alert("Fehler bei der Registrierung: " + error.message);
         });
 }
+
